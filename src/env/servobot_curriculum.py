2533a90df60852e1155c590ff74fa6bb565101a8
Clay Goldsmith <buzzscoutbark@gmail.com>
2026-02-03 18:47:45 -0600

    beginnings of curriculum

---
+++ src/env/servobot_curriculum.py
@@ -0,0 +1,37 @@
+
+from env.servobot import ServobotEnv
+
+
+class ServobotCurriculumEnv(ServobotEnv):
+    def __init__(
+        self,
+        num_envs,
+        env_cfg,
+        obs_cfg,
+        reward_cfg,
+        command_cfg,
+        headless: bool = False,
+        debug: bool = False,
+    ):
+        super().__init__(
+            num_envs, env_cfg, obs_cfg, reward_cfg, command_cfg, headless, debug
+        )
+        self.curriculum_enabled = env_cfg.get("curriculum_enabled", True)
+        self.curriculum_steps = env_cfg.get("curriculum_steps", 1000)
+        self.current_step = 0
+
+
+''' 
+Curriculum.yaml
+ 
+config1:
+    name: "Easy"
+    env: 
+        blah blah blah
+    reward:
+        blah blah blah
+        value 
+        
+    commands:
+        blah blah blah
+'''
